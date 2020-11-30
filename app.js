70741acd5c0b111b2c07c816329dfa04307ae85f
Rockfishroll91 <aodoyle91@gmail.com>
2020-11-30 14:28:37 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Hello World!"); var canvas = document.querySelector("canvas"); var c = canvas.getContext("2d"); canvas.width = innerWidth; canvas.height = innerHeight; canvas.style.backgroundColor = "black";
+
+function Bricks() {
+    
+} 
+
+function init() {
+
+} init();
